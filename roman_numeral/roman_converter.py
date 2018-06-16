24a2f18dc1c336bbc63e82ed14c8779eca161bfc
Nicolas Knoebber <knoebber@gmail.com>
2018-06-16 10:38:34 -0700

    add method signatures

---
+++ roman_numeral/roman_converter.py
@@ -0,0 +1,13 @@
+"""
+converts an integer i into a roman numeral
+precondition: i < 4000
+"""
+
+def to_roman(i) :
+  return ''
+
+"""
+converts a roman numeral string s to an integer
+"""
+def to_int(s) :
+  return 0
