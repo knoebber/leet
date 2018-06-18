1a8d342a385f3d84864d87022069f40090b7a279
Nicolas Knoebber <knoebber@gmail.com>
2018-06-17 21:12:01 -0700

    clean, add to_int , add test

--- roman_numeral/roman_converter.py
+++ roman_numeral/roman_converter.py
@@ -1,8 +1,8 @@
 symbols = {
-  0:['I','V'],
-  1:['X','L'],
-  2:['C','D'],
-  3:['M']
+  0 : ['I','V'],
+  1 : ['X','L'],
+  2 : ['C','D'],
+  3 : ['M']
 }
 
 """
@@ -16,37 +16,44 @@ precondition: i < 4000
 def to_roman(i) :
   return _to_roman(0,i)
 
-def _to_roman(tens,i,debug=False) :
-  if debug:
-    print 'tens: ',tens
-    print 'i: ',i
-  if i == 0:
-    if debug:
-      print 'i == 0'
-    return ''
+def _to_roman(tens,i,) :
+  if i == 0: return ''
+
   f = lambda: _to_roman(tens+1,i/10)
   d = i%10
-  if debug:
-    print 'digit: ',d
-    print 'new i: ',i
 
-  if d == 0 :
-    if debug:
-      print 'd == 0' 
-    return f()
-  if d == 4 :
-    return f() + symbols[tens][0] + symbols[tens][1]
-  elif d == 9 :
-    return f() + symbols[tens][0] + symbols[tens+1][0]
-  elif d < 5 :
-    return f() + symbols[tens][0]*d
-  elif d == 5 :
-    return f() + symbols[tens][1]
-  else :
-    return f() + symbols[tens][1] + (symbols[tens][0]*(d-5))
+  if d == 0   : return f()
+  if d == 4   : return f() + symbols[tens][0] + symbols[tens][1]
+  elif d == 9 : return f() + symbols[tens][0] + symbols[tens+1][0]
+  elif d < 5  : return f() + symbols[tens][0] * d
+  elif d == 5 : return f() + symbols[tens][1]
+  else        : return f() + symbols[tens][1] + (symbols[tens][0]*(d-5))
 
+values = {
+  'I'  : 1,
+  'IV' : 4,
+  'V'  : 5,
+  'IX' : 9,
+  'X'  : 10,
+  'XL' : 40,
+  'L'  : 50,
+  'XC' : 90,
+  'C'  : 100,
+  'CD' : 400,
+  'D'  : 500,
+  'CM' : 900,
+  'M'  : 1000
+}
 """
 converts a roman numeral string s to an integer
 """
 def to_int(s) :
-  return 0
+  i = 0
+  while len(s) > 0 :
+    if s[:2] in values :
+      i += values[s[:2]]
+      s = s[2:]
+    else :
+      i += values[s[0]]
+      s = s[1:]
+  return i
--- roman_numeral/test.py
+++ roman_numeral/test.py
@@ -1,7 +1,33 @@
 from roman_converter import to_roman,to_int
 
+"""
+test values < 100
+"""
+def to_roman_small() :
+  assert to_roman(1)  == 'I'
+  assert to_roman(2)  == 'II'
+  assert to_roman(3)  == 'III'
+  assert to_roman(4)  == 'IV'
+  assert to_roman(9)  == 'IX'
+  assert to_roman(11) == 'XI'
+  assert to_roman(14) == 'XIV'
+  assert to_roman(19) == 'XIX'
+  assert to_roman(21) == 'XXI'
+  assert to_roman(40) == 'XL'
+  assert to_roman(44) == 'XLIV'
+  assert to_roman(43) == 'XLIII'
+  assert to_roman(51) == 'LI'
+  assert to_roman(59) == 'LIX'
+  assert to_roman(78) == 'LXXVIII'
+  assert to_roman(90) == 'XC'
+  assert to_roman(94) == 'XCIV'
+  assert to_roman(96) == 'XCVI'
+  assert to_roman(99) == 'XCIX'
+  print 'pass small test'
 
-
+"""
+test basic cases
+"""
 def to_roman_basic() :
   assert to_roman(1)    == 'I'
   assert to_roman(5)    == 'V'
@@ -10,9 +36,18 @@ def to_roman_basic() :
   assert to_roman(100)  == 'C'
   assert to_roman(500)  == 'D'
   assert to_roman(1000) == 'M'
+  print 'pass basic to roman tests'
 
-def test_to_roman() :
+def test_to_int() :
+  for i in range(0,4000) :
+    roman = to_roman(i)
+    convt = to_int(roman)
+    assert convt == i
+
+def test_roman_conversion() :
+  to_roman_small()
   to_roman_basic()
+  test_to_int()
   print 'all tests pass!'
 
-test_to_roman()
+test_roman_conversion()
