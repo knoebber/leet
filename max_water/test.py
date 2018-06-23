7888acd38d70ed9998a2b64045cc5aca9de1db0b
Nicolas Knoebber <knoebber@gmail.com>
2018-06-23 10:32:01 -0700

    add test that proves efficiency

--- max_water/test.py
+++ max_water/test.py
@@ -1,16 +1,27 @@
 from calc import calculate,f_calc
 from random import randint
+from time import time
 
 def test_versions() :
-  iterations = 100
-  max_length = 20
+  iterations = 10
+  length = 1000
   max_size   = 50
   for i in range(0,iterations) :
     l = []
-    for j in range(0,max_length) :
+    for j in range(0,length) :
       l.append(randint(1,max_size))
+    start = time()
+    naive = calculate(l)
+    end = time()
+    elapsed = end - start
+    print 'naive took: ', elapsed
+    start = time()
+    optimized = f_calc(l)
+    end = time()
+    elapsed = end - start
+    print 'optimized took: ', elapsed
     try :
-      assert calculate(l) == f_calc(l)
+      assert naive == optimized
     except :
       print 'failed on: ',l
       raise Exception ('Failed test versions')
