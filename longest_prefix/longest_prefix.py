34974b1d58772954eb6cedd84cde4f7a6fa4c6fd
Nicolas Knoebber <knoebber@gmail.com>
2018-06-24 11:43:51 -0700

    clean and add edge cases

--- longest_prefix/longest_prefix.py
+++ longest_prefix/longest_prefix.py
@@ -3,17 +3,21 @@ find longest prefix
 """
 def f_l_p(strings) :
   r = ''
+  if not strings or not strings[0] : return ''
   c = strings[0][0]
   i = 1 #word to check
   j = 0 #char to check
+  l = len(strings)
   while True :
-    if i > len(strings) - 1 :
+    if i > l - 1 :
       i = 0
       j += 1
       r += c
       if j > len(strings[i]) - 1 :
         return r
       c = strings[i][j]
+    if j > len(strings[i]) - 1 :
+      return r
     if c != strings[i][j] :
       return r
     i = i + 1
--- longest_prefix/test.py
+++ longest_prefix/test.py
@@ -1,14 +1,15 @@
 from longest_prefix import f_l_p
 
-
 def test_flp() :
   assert f_l_p(['a']) == 'a'
   assert f_l_p(['a','a']) == 'a'
+  assert f_l_p(['aa','a']) == 'a'
   assert f_l_p(['aa','aax']) == 'aa'
   assert f_l_p(['aabazb','aax']) == 'aa'
   assert f_l_p(['aabazb','aabasl']) == 'aaba'
   assert f_l_p(['dog','racecar','car']) == ''
   assert f_l_p(['flower','flow','flight']) == 'fl'
   print 'passes!'
+
 
 test_flp()
